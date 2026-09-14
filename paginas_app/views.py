7e83fa018cf184b83b4665a123e1907aea9782e0
Laboratorio 117 <CAL_LAB117@inacap.cl>
2026-09-14 12:09:30 -0300

    modificar paginas_app2

--- paginas_app/views.py
+++ paginas_app/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
-def mostrar_home(requests):
-    return render(requests, 'inicio.html')
+def mostrar_home(request):
+    return render(request, 'inicio.html')
 
-def mostrar_acerca(requests):
-    datos: {
-        "nombre": "lavado de vehículos",
-        "valor": 10000
+def mostrar_acerca(request):
+    return render(request, 'acerca-de.html')
+
+def mostrar_servicio(request):
+    datos = {
+        'nombre': 'Lavado de Vehiculos',
+        'valor': 10000
     }
+    return render(request, 'acerca-de.html', datos)
